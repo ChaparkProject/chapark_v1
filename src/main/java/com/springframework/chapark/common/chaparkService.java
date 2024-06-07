a2a6b6d7c28a7808543866f2c40caa5beda0e42a
CEO <CEO@192.168.50.181>
2024-06-07 13:57:20 +0900

    *dao 로직 구현

--- src/main/java/com/springframework/chapark/common/chaparkService.java
+++ src/main/java/com/springframework/chapark/common/chaparkService.java
@@ -1,58 +1,54 @@
 package com.springframework.chapark.common;
 
-import org.apache.ibatis.session.SqlSession;
-import org.springframework.beans.factory.annotation.Autowired;
 import java.util.List;
 import java.util.Map;
 
-public class ChaparkDAO {
+import org.springframework.beans.factory.annotation.Autowired;
 
-	private SqlSession sqlSession;
+public class chaparkService {
+	private final ChaparkDAO chaparkDAO;
 
 	@Autowired
-	public ChaparkDAO(SqlSession sqlSession) {
-		this.sqlSession = sqlSession;
+	public chaparkService(ChaparkDAO chaparkDAO) {
+		this.chaparkDAO = chaparkDAO;
 	}
 
 	// 단건 조회 (Map)
 	public Map<String, Object> selectMap(Map<String, Object> paramMap, String mapperName, String sqlId) {
-		String sql = mapperName + "." + sqlId;
-		return sqlSession.selectOne(sql, paramMap);
+		return chaparkDAO.selectMap(paramMap, mapperName, sqlId);
 	}
 
 	// 단건 조회 (Object)
 	public Object selectObject(Map<String, Object> paramMap, String mapperName, String sqlId) {
-		String sql = mapperName + "." + sqlId;
-		return sqlSession.selectOne(sql, paramMap);
+		return chaparkDAO.selectObject(paramMap, mapperName, sqlId);
 	}
 
 	// 다건 조회 (페이징 처리)
 	// 추후 수정 예정
 	public List<Map<String, Object>> selectPaging(Map<String, Object> paramMap) {
-		return sqlSession.selectList("namespace.selectMultiplePaged", paramMap);
+		// 여기에 페이징 처리 로직 추가
+		return chaparkDAO.selectPaging(paramMap);
 	}
 
 	// 다건 조회 (List)
 	// 추후 수정 예정
 	public List<Map<String, Object>> selectList(Map<String, Object> paramMap) {
-		return sqlSession.selectList("namespace.selectMultipleList", paramMap);
+		// 여기에 다건 조회 로직 추가
+		return chaparkDAO.selectList(paramMap);
 	}
 
 	// 삽입
 	public int insert(Map<String, Object> paramMap, String mapperName, String sqlId) {
-		String sql = mapperName + "." + sqlId;
-		return sqlSession.insert(sql, paramMap);
+		return chaparkDAO.insert(paramMap, mapperName, sqlId);
 	}
 
 	// 업데이트
 	public int update(Map<String, Object> paramMap, String mapperName, String sqlId) {
-		String sql = mapperName + "." + sqlId;
-		return sqlSession.update(sql, paramMap);
+		return chaparkDAO.update(paramMap, mapperName, sqlId);
 	}
 
 	// 삭제
 	public int delete(Map<String, Object> paramMap, String mapperName, String sqlId) {
-		String sql = mapperName + "." + sqlId;
-		return sqlSession.delete(sql, paramMap);
+		return chaparkDAO.delete(paramMap, mapperName, sqlId);
 	}
 }
