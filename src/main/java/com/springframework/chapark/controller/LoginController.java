865b7bb51536ed2c31d6aa9e342a45f0ed84a922
kdh56 <kdh56@KANGDONGHEE>
2024-06-28 21:42:10 +0900

    /*앙 몰랑 커밋*/

--- src/main/java/com/springframework/chapark/controller/LoginController.java
+++ src/main/java/com/springframework/chapark/controller/LoginController.java
@@ -14,6 +14,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.springframework.chapark.common.ChaparkService;
 
@@ -24,11 +26,12 @@ public class LoginController {
 	@Autowired
 	private ChaparkService ChaparkService;
 
-	@SuppressWarnings("rawtypes" )
+	@SuppressWarnings({ "rawtypes", "unchecked" } )
 	@RequestMapping(value = "/login.do")
-	public String loginMain(Map paramMap, HttpServletRequest request) throws Exception {
-			//paramMap.put("mberId", "test1");
-			//paramMap.put("mberPw", "test1");
+
+	public String loginMain(@RequestParam Map<String, Object> paramMap, HttpServletRequest request) throws Exception {
+			paramMap.put("mberId",(String) paramMap.get("mberId"));
+			paramMap.put("mberPw",(String) paramMap.get("mberPw"));
 			
 			Map memberInfo = ChaparkService.selectMap( "lo_login.selectUserInfo", paramMap);
 
@@ -37,10 +40,11 @@ public class LoginController {
 					HttpSession session = request.getSession();
 					session.setAttribute("memberInfo", memberInfo);
 					session.setMaxInactiveInterval(60 * 30); // 세션 유지시간 30분 설정, 60초 * 30분 = 1800초
+					return "WEB-INF/views/mberTest";
 			} else { // 로그인 정보가 없을 시
-				return "mberTest";
+				return "login";
 			}
-		return "login";
+		//return "login";
 	}
 
 	@RequestMapping(value = "/logout.do")
--- src/main/webapp/WEB-INF/views/login.jsp
+++ src/main/webapp/WEB-INF/views/login.jsp
@@ -3,6 +3,7 @@
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt"%>
 
+<script  src="http://code.jquery.com/jquery-latest.min.js"></script>
 <script>
 	// 로그인 불가 alert 처리
 	//const msg = "<c:out value='${msg}'/>";
@@ -24,21 +25,21 @@
 		
 		$.ajax({
 			type : "POST", 
-			url : "/login.do", 
+			url : "/chapark/login.do", 
 			data : {
-				mberId : $('#mberId').val(),
-				mberPw : $('#mberPw').val() 
+				'mberId' : $('#mberId').val(),
+				'mberPw' : $('#mberPw').val() 
 			},
 			success : function(result) {
-				if(result == 'true') {
-					location.href = "/mber.do";
+				if(result) {
+					location.href = "/chapark/mber.do";
 				} else {
 					alert('아이디와 비밀번호가 일치하지 않습니다.');
 					$('#mberId').focus;
 				}
 			},
-			error: function(error) {
-				alert(error); 
+			error: function() {
+				alert("오류가 발생했습니다."); 
 			}
 		});
 	}
@@ -49,5 +50,6 @@
 		<input type="text" id="mberId" placeholder="아이디" /> 
 		<input type="password" id="mberPw" placeholder="비밀번호" />
 		<a onclick="login()">로그인</a>
+		<!--  <button onclick="login()">로그인</button> -->
 	</div>
 </form>
