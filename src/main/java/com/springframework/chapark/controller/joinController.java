b389165e7786a06476f810d9029c715affcca55d
CEO <CEO@192.168.50.181>
2024-07-19 10:59:04 +0900

    비밀번호 암호화

--- src/main/java/com/springframework/chapark/controller/joinController.java
+++ src/main/java/com/springframework/chapark/controller/joinController.java
@@ -3,6 +3,9 @@ package com.springframework.chapark.controller;
 import com.springframework.chapark.common.ChaparkLogger;
 import com.springframework.chapark.common.ChaparkService;
 import com.springframework.chapark.common.CommonMap;
+import com.springframework.chapark.security.Encryption;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,6 +22,9 @@ public class joinController {
 
 	@Autowired
 	private ChaparkService chaparkService;
+	
+	@Autowired
+	private Encryption encryption;
 
 
 	@GetMapping("/join.do")
@@ -26,10 +32,21 @@ public class joinController {
 		return "join";
 	}
 
+	@SuppressWarnings("static-access")
 	@PostMapping("/join.do")
-	public String join(CommonMap commonMap, Model model) {
+	public String join(CommonMap commonMap, Model model, HttpServletRequest request) {
 		try {
-			chaparkService.insert("jo_join.insertMberJoin",commonMap.getMap());
+			// 암호 확인
+			//System.out.println("첫번째:" + userVO.getUser_pw());
+			// 비밀번호 암호화 (sha-256)
+			String encryPassword = encryption.encrypt(commonMap.get("MBER_PW").toString()); // map에서 가져온 비밀번호를 암호화
+			commonMap.put("MBER_PW",encryPassword); //암호화한 비밀번호 map에 다시 넣기
+			//System.out.println("두번째:" + commonMap.getUser_pw());
+			// 회원가입 메서드
+			chaparkService.insert("jo_join.insertMberJoin",commonMap.getMap()); //회원가입 insert
+			// 인증 메일 보내기 메서드
+			//mailsender.mailSendWithUserKey(userVO.getUser_email(), userVO.getUser_id(), request);
+			
 
 		} catch (Exception e) {
 			ChaparkLogger.debug(e, this.getClass(), "join");
