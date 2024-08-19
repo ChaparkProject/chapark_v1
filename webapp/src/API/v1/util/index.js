15f4cea2bfd28be06bde5110198b22df464557a4
yhju <jyh000823@naver.com>
2024-08-20 02:17:57 +0900

    수정

--- webapp/src/API/v1/util/index.js
+++ webapp/src/API/v1/util/index.js
@@ -1,8 +1,6 @@
 import axios from 'axios';
 
-const axiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL
-});
+const axiosInstance = axios.create({});
 
 export const Get = async (url, config) => {
   const response = await axiosInstance.get(url, config);
@@ -10,17 +8,17 @@ export const Get = async (url, config) => {
 };
 
 export const Post = async (url, data, config) => {
-  const response = await axiosInstance.post(url, JSON.stringify(data), config);
+  const response = await axiosInstance.post(url, data, config);
   return response;
 };
 
 export const Put = async (url, data, config) => {
-  const response = await axiosInstance.put(url, JSON.stringify(data), config);
+  const response = await axiosInstance.put(url, data, config);
   return response;
 };
 
 export const Patch = async (url, data, config) => {
-  const response = await axiosInstance.patch(url, JSON.stringify(data), config);
+  const response = await axiosInstance.patch(url, data, config);
   return response;
 };
 
